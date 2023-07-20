3af76dce47780a7613125a357130a9cccfada80a
Rotimi Joseph <Rotimi@rotimis-mbp.home>
2023-07-20 23:04:31 +0100

    remove all comments

--- StudentAndSubjectApp/Controllers/StudentController.cs
+++ StudentAndSubjectApp/Controllers/StudentController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using StudentAndSubjectApp.Models;
 
 namespace StudentAndSubjectApp.Controllers
 {
     public class StudentController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
             List<Student> students = new List<Student>()
