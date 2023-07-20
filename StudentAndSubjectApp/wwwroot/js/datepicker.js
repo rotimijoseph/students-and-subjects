ae0bb6c3b4f9946542bf85d7030a750d731cb466
Rotimi Joseph <Rotimi@rotimis-mbp.home>
2023-07-20 16:51:15 +0100

    add ability to create a new account and fix issue with the date picker

---
+++ StudentAndSubjectApp/wwwroot/js/datepicker.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $('#datepicker').datepicker({
+        format: 'yyyy-mm-dd',
+        minViewMode: 'days'
+    });
+});
---
+++ StudentAndSubjectApp/wwwroot/js/validateInputValue.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $('#studentForm').submit(function () {
+        var inputValue = $('#firstName').val();
+
+        if (inputValue.length > 25) {
+            alert("Input value for 'First name' cannot exceed 25 characters.");
+            return false;
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#studentForm').submit(function () {
+        var inputValue = $('#surname').val();
+
+        if (inputValue.length > 25) {
+            alert("Input value for 'Surname' cannot exceed 25 characters.")
+            return false;
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#studentForm').submit(function () {
+        var inputValue = $('#emailAddress').val();
+
+        if (inputValue.length > 50) {
+            alert("Input value for your 'Email address' cannot exceed 50 characters.");
+            return false;
+        }
+    });
+});
