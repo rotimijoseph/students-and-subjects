2f3e6f51c91d86dd6f307ca4d4b47e2a90e97f94
Rotimi Joseph <Rotimi@rotimis-mbp.home>
2023-07-20 16:57:35 +0100

    add when account is created redirected to new page confirming submission

--- StudentAndSubjectApp/Controllers/NewAccountController.cs
+++ StudentAndSubjectApp/Controllers/NewAccountController.cs
@@ -15,6 +15,11 @@ namespace StudentAndSubjectApp.Controllers
         {
             return View();
         }
+
+        public IActionResult AccountCreated()
+        {
+            return View();
+        }
     }
 }
 
